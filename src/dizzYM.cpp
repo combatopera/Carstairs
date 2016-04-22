9a68c2a662323dc829221a5de23a45a0d04a4869
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-04-22 23:41:17 +0100

    extract h file

--- src/dizzYM.cpp
+++ src/dizzYM.cpp
@@ -1,79 +1,15 @@
+#include "dizzYM.h"
+
 #include <alsa/seq_event.h>
 #include <dssi.h>
 #include <ladspa.h>
 #include <math.h>
 #include <stddef.h>
-#include <stdlib.h>
+#include <cstdlib>
 
 #ifdef DEBUG_dizzYM
 #include <iostream>
 #endif
-
-class dizzYM {
-
-    dizzYM(int sampleRate);
-
-    ~dizzYM();
-
-    enum {
-
-        OutputPort = 0, Sustain = 1, PortCount = 2
-
-    };
-
-    enum {
-
-        Notes = 128
-
-    };
-
-    static const char * const PortNames[PortCount];
-
-    static const LADSPA_PortDescriptor PortDescriptors[PortCount];
-
-    static const LADSPA_PortRangeHint PortRangeHints[PortCount];
-
-    static const LADSPA_Properties Properties;
-
-    static const LADSPA_Descriptor ladspaDescriptor;
-
-    static LADSPA_Handle instantiate(const LADSPA_Descriptor *, unsigned long);
-    static void connect_port(LADSPA_Handle, unsigned long, LADSPA_Data *);
-    static void activate(LADSPA_Handle);
-    static void run(LADSPA_Handle, unsigned long);
-    static void deactivate(LADSPA_Handle);
-    static void cleanup(LADSPA_Handle);
-    static const DSSI_Program_Descriptor *getProgram(LADSPA_Handle, unsigned long);
-    static void selectProgram(LADSPA_Handle, unsigned long, unsigned long);
-    static int get_midi_controller_for_port(LADSPA_Handle, unsigned long);
-    static void run_synth(LADSPA_Handle, unsigned long, snd_seq_event_t *, unsigned long);
-
-    void runImpl(unsigned long, snd_seq_event_t *, unsigned long);
-    void addSamples(int, unsigned long, unsigned long);
-
-    float *_output;
-
-    float *_sustain;
-
-    int _sampleRate;
-
-    long _blockStart;
-
-    long _ons[Notes];
-
-    long _offs[Notes];
-
-    int _velocities[Notes];
-
-    float *_wavetable[Notes];
-
-    float _sizes[Notes];
-
-public:
-
-    static const DSSI_Descriptor dssiDescriptor;
-
-};
 
 const char * const dizzYM::PortNames[PortCount] = { //
         "Output", //
