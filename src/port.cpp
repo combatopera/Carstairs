02a3e04aed8431be78eacec064956484ddff6c9c
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-04-23 14:17:57 +0100

    so that's how it's done

--- src/port.cpp
+++ src/port.cpp
@@ -4,10 +4,7 @@
 
 Port::Port(const char *name, LADSPA_PortDescriptor descriptor, LADSPA_PortRangeHintDescriptor HintDescriptor, LADSPA_Data LowerBound, LADSPA_Data UpperBound)
         : _name(name), _descriptor(descriptor) {
-    _rangeHintPtr = new LADSPA_PortRangeHint;
-    _rangeHintPtr->HintDescriptor = HintDescriptor;
-    _rangeHintPtr->LowerBound = LowerBound;
-    _rangeHintPtr->UpperBound = UpperBound;
+    _rangeHintPtr = new LADSPA_PortRangeHint{HintDescriptor, LowerBound, UpperBound};
 }
 
 Port::~Port() {
