de2b4e1ac169211dd65ba65179f43779e12b2f1f
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-05-01 16:12:13 +0100

    undup

--- src/util.cpp
+++ src/util.cpp
@@ -4,17 +4,13 @@
 #include <stddef.h>
 #include <cstring>
 
-template<typename T>
-void zero(T *buf, size_t n) {
+template<typename T> void zero(T *buf, size_t n) {
     memset(buf, 0, n * sizeof *buf); // Not portable in float case.
 }
 
-template<typename T>
-void copy(T *from, T *to, size_t n) {
+template<typename T> void copy(T *from, T *to, size_t n) {
     memcpy(to, from, n * sizeof *from);
 }
-template void copy<int>(int *,int *,size_t n);
-template void copy<LADSPA_Data>(LADSPA_Data *,LADSPA_Data *,size_t n);
 
-template void zero<int>(int *,size_t n);
-template void zero<LADSPA_Data>(LADSPA_Data *,size_t n);
+INSTANTIATE(int)
+INSTANTIATE(LADSPA_Data)
--- src/util.h
+++ src/util.h
@@ -1,8 +1,11 @@
-#include <stddef.h>
 #pragma once
-template<typename T>
-void zero(T *buf, size_t n);
 
-template<typename T>
-void copy(T *from, T *to, size_t n);
+#include <stddef.h>
 
+template<typename T> void zero(T *buf, size_t n);
+
+template<typename T> void copy(T *from, T *to, size_t n);
+
+#define INSTANTIATE(T) \
+    template void zero<T>(T *, size_t n); \
+    template void copy<T>(T *, T *, size_t n);
