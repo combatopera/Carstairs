8b80e1d85829f47cb023db5230732ce6e1ce5046
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-05-02 10:51:12 +0100

    correcter

--- src/dizzYM.cpp
+++ src/dizzYM.cpp
@@ -11,7 +11,7 @@ PortInfoEnum::PortInfoEnum(int ord)
         SUSTAIN {ord++, false, false, "Sustain (on/off)",
         LADSPA_HINT_DEFAULT_MINIMUM | LADSPA_HINT_INTEGER | LADSPA_HINT_BOUNDED_BELOW | LADSPA_HINT_BOUNDED_ABOVE, 0, 1, DSSI_CC(64)}, //
         _values {&OUTPUT, &SUSTAIN}, //
-        _count(ord) {
+        _count(ord - OUTPUT._ordinal) {
 }
 
 dizzYM::dizzYM(int sampleRate)
