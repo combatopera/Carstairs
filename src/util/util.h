52797ed53172e906331063d27b144a5b443b1483
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-05-13 23:40:33 +0100

    bounds class

--- src/util/util.h
+++ src/util/util.h
@@ -8,3 +8,19 @@ typedef unsigned long cursor_t;
 cursor_t const CURSOR_MAX = ULONG_MAX;
 
 void debug(const char *format, ...);
+
+template<typename T> class Bounds {
+
+public:
+
+    T const _min, _max;
+
+    Bounds(T min, T max)
+            : _min(min), _max(max) {
+    }
+
+    T clamp(T x) const {
+        return _min <= x ? (x <= _max ? x : _max) : _min;
+    }
+
+};
