8c66bf564146efec34ce11b47ae0487f211fe312
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-05-16 20:23:17 +0100

    i want to see it during tests for example

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -4,12 +4,10 @@
 #include <cstdio>
 
 void debug(char const *format, ...) {
-#ifdef DEBUG_dizzYM
     fprintf(stderr, "[dizzYM] ");
     va_list ap;
     va_start(ap, format);
     vfprintf(stderr, format, ap);
     va_end(ap);
     fprintf(stderr, "\n");
-#endif
 }
