3d2bf25d56eb33f432d107fd7b57358ab13b3c52
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-05-17 22:19:58 +0100

    derivative buffer

--- src/node/pcm.cpp
+++ src/node/pcm.cpp
@@ -9,17 +9,23 @@
 #include "../util/util.h"
 
 PCM::PCM(Config const *config, State *state, Node<float> *naive)
-        : Node("PCM", state), _minBLEPs(config), _naive(naive) {
+        : Node("PCM", state), _minBLEPs(config), _naive(naive), _dc(INITIAL_DC) {
     // Nothing else.
 }
 
 void PCM::resetImpl() {
-    // Nothing to be done.
+    _dc = INITIAL_DC;
 }
 
 void PCM::renderImpl() {
     size_t pcmCount = _buf.limit();
     cursor_t naiveX = _naive->cursor(), naiveN = _minBLEPs.getMinNaiveN(naiveX, pcmCount);
-    View<float> v = _naive->render(naiveX + naiveN);
-    _minBLEPs.paste(naiveX, v, _buf);
+    View<float> naive = _naive->render(naiveX + naiveN);
+    _derivative.setLimit(naive.limit());
+    _derivative.fill(naive.begin());
+    _derivative.differentiate(_dc);
+    if (naive.limit()) { // Otherwise _dc doesn't change.
+        _dc = naive.at(naive.limit() - 1);
+    }
+    _minBLEPs.paste(naiveX, naive, _buf);
 }
--- src/node/pcm.h
+++ src/node/pcm.h
@@ -5,13 +5,20 @@
 #include "../config.h"
 #include "../node.h"
 #include "../state.h"
+#include "../util/buf.h"
 #include "../util/minbleps.h"
 
 class PCM: public Node<LADSPA_Data> {
 
+    constexpr static float INITIAL_DC = 0;
+
     MinBLEPs const _minBLEPs;
 
     Node<float> * const _naive;
+
+    Buffer<float> _derivative {"_derivative"};
+
+    float _dc;
 
     void resetImpl();
 
