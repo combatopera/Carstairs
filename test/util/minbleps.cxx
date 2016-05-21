6b0dfbf96c560de3fb64a5f65ff4763cb5af5e94
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-05-21 12:59:51 +0100

    testing

--- test/util/minbleps.cxx
+++ test/util/minbleps.cxx
@@ -5,7 +5,7 @@
 
 BOOST_AUTO_TEST_SUITE(TestMinBLEPs)
 
-BOOST_AUTO_TEST_CASE(minBLEPSizeWorks) {
+BOOST_AUTO_TEST_CASE(minBLEPSize) {
     Config config(6);
     config._nominalClock = 15;
     MinBLEPs minBLEPs(&config);
@@ -16,6 +16,18 @@ BOOST_AUTO_TEST_CASE(minBLEPSizeWorks) {
     BOOST_REQUIRE_EQUAL(102, minBLEPs.minBLEPSize(2));
     BOOST_REQUIRE_EQUAL(102, minBLEPs.minBLEPSize(3));
     BOOST_REQUIRE_EQUAL(102, minBLEPs.minBLEPSize(4));
+}
+
+BOOST_AUTO_TEST_CASE(pcmXToNaiveX) {
+    Config config(6);
+    config._nominalClock = 15;
+    MinBLEPs minBLEPs(&config);
+    BOOST_REQUIRE_EQUAL(1, minBLEPs.pcmXToNaiveX(1));
+    BOOST_REQUIRE_EQUAL(3, minBLEPs.pcmXToNaiveX(2));
+    BOOST_REQUIRE_EQUAL(6, minBLEPs.pcmXToNaiveX(3));
+    BOOST_REQUIRE_EQUAL(8, minBLEPs.pcmXToNaiveX(4));
+    BOOST_REQUIRE_EQUAL(11, minBLEPs.pcmXToNaiveX(5));
+    BOOST_REQUIRE_EQUAL(13, minBLEPs.pcmXToNaiveX(6));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
