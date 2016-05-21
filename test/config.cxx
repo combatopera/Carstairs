7407e61137145f5c078136e6d32007764044d1fe
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-05-21 15:49:18 +0100

    testing

---
+++ test/config.cxx
@@ -0,0 +1,22 @@
+#include "../src/config.h"
+
+#include <boost/test/test_tools.hpp>
+#include <boost/test/unit_test_suite.hpp>
+
+BOOST_AUTO_TEST_SUITE(TestConfig)
+
+BOOST_AUTO_TEST_CASE(evenEmpiricalOrder) {
+    Config config(44100);
+    config._transition = .05;
+    BOOST_REQUIRE_EQUAL(80, config.evenEmpiricalOrder());
+    config._transition = .0505;
+    BOOST_REQUIRE_EQUAL(80, config.evenEmpiricalOrder());
+    config._transition = .051;
+    BOOST_REQUIRE_EQUAL(78, config.evenEmpiricalOrder());
+    config._transition = .0495;
+    BOOST_REQUIRE_EQUAL(80, config.evenEmpiricalOrder());
+    config._transition = .049;
+    BOOST_REQUIRE_EQUAL(82, config.evenEmpiricalOrder());
+}
+
+BOOST_AUTO_TEST_SUITE_END()
