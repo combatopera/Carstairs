e25cf404024fb268c85a9e59c6c3d1f87bcf78b2
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-05-25 22:59:32 +0100

    pcmRate no longer in the config

--- src/config.h
+++ src/config.h
@@ -27,8 +27,6 @@ public:
 
     int final _nominalClock = 2000000; // TODO LATER: Support non-integer clock.
 
-    int const _pcmRate; // DSSI doesn't support non-integer rates.
-
     double final _cutoff = .475, _transition = .05; // Both normalised to rate so in [0, .5].
 
     double final _rcepsAddBeforeLog = 1e-50;
@@ -37,9 +35,7 @@ public:
 
     float const _maxAmp = .5;
 
-    Config(int pcmRate)
-            : _pcmRate(pcmRate) {
-    }
+    int const _alphaCC = 80, _betaCC = 81;
 
     int naiveRate() const {
         assert(!((_nominalClock * _atomSize) % YM2149_ATOM_SIZE));
@@ -51,9 +47,9 @@ public:
         return int(round(empirical / 2)) * 2; // Closest even int to empirical.
     }
 
-    int idealMinBLEPCount() const {
-        int naiveRate = this->naiveRate();
-        return naiveRate / boost::math::gcd(naiveRate, _pcmRate);
+    int idealMinBLEPCount(int const pcmRate) const {
+        int const naiveRate = this->naiveRate();
+        return naiveRate / boost::math::gcd(naiveRate, pcmRate);
     }
 
 };
