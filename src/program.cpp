e8dc83d017160f28c943fa4f1d6ca54ea4cd9731
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-05-28 18:33:17 +0100

    const

--- src/program.cpp
+++ src/program.cpp
@@ -1,12 +1,12 @@
 #include "program.h"
 
 void Program::fire(DSSI::cursor cursor) {
-    auto onOrMax = _state.onOrMax();
+    auto const onOrMax = _state.onOrMax();
     if (DSSI::CURSOR_MAX == onOrMax || cursor < onOrMax) {
         _state.setLevel4(0); // Never been on or not yet on.
     }
     else {
-        auto offOrMax = _state.offOrMax();
+        auto const offOrMax = _state.offOrMax();
         if (DSSI::CURSOR_MAX == offOrMax || cursor < offOrMax) {
             _state.setLevel4(0xf); // Not yet off.
         }
