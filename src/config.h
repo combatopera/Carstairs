5b4fe95cb54525aec17d81d8b4fa5818954aface
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-05-28 17:55:09 +0100

    use correct jargon

--- src/config.h
+++ src/config.h
@@ -27,7 +27,7 @@ public:
 
     float final _nominalClock = 2000000;
 
-    double final _transition = .1, _void = 0; // Both normalised to rate so in [0, .5].
+    double final _transition = .1, _stopband = 0; // Both normalised to rate so in [0, .5].
 
     double final _rcepsAddBeforeLog = 1e-50;
 
@@ -47,7 +47,7 @@ public:
     }
 
     double cutoff() const {
-        return .5 - _void - _transition / 2;
+        return .5 - _stopband - _transition / 2;
     }
 
     int idealMinBLEPCount(int const pcmRate) const {
