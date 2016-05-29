4f3eb8e29298708efb3bdbb74f447f32fc475be3
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-05-29 21:19:22 +0100

    inline for now

--- src/program.h
+++ src/program.h
@@ -20,7 +20,14 @@ class Program: public Fire {
 
     float _rate = DEFAULT_RATE;
 
-    void load() {
+public:
+
+    Program(Config const& config)
+            : _moduleName(config._programModule) {
+        debug("Initing Python.");
+        Py_InitializeEx(0);
+        _parent = PyThreadState_Get();
+        assert(_parent);
         debug("Creating new sub-interpreter.");
         _interpreter = Py_NewInterpreter();
         assert(_interpreter);
@@ -39,28 +46,6 @@ class Program: public Fire {
         }
     }
 
-    void unload() {
-        debug("Ending sub-interpreter.");
-        _module = _path = 0;
-        Py_EndInterpreter(_interpreter);
-    }
-
-public:
-
-    Program(Config const& config)
-            : _moduleName(config._programModule) {
-        debug("Initing Python.");
-        Py_InitializeEx(0);
-        _parent = PyThreadState_Get();
-        assert(_parent);
-        load();
-    }
-
-    void reload() {
-        unload();
-        load();
-    }
-
     float rate() const {
         return _rate;
     }
@@ -68,7 +53,9 @@ public:
     void fire(int, int, State&) const;
 
     ~Program() {
-        unload();
+        debug("Ending sub-interpreter.");
+        _module = _path = 0;
+        Py_EndInterpreter(_interpreter);
         PyThreadState_Swap(_parent); // Otherwise Py_Finalize crashes.
         debug("Closing Python.");
         Py_Finalize();
