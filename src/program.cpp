22b340eff1aebf3bb6dd52967436d740bf18349c
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-05-29 08:56:56 +0100

    it's a start

--- src/program.cpp
+++ src/program.cpp
@@ -1,8 +1,13 @@
 #include "program.h"
 
+#include <algorithm>
+#include <array>
+
+static std::array<int, 10> ENVELOPE {13, 14, 15, 14, 13, 12, 11, 10, 9, 8};
+
 void Program::fire(int noteFrame, int offFrameOrNeg, State& state) const {
     if (offFrameOrNeg < 0) {
-        state.setLevel4(0xf);
+        state.setLevel4(ENVELOPE[std::min(noteFrame, int(ENVELOPE.size() - 1))]);
     }
     else {
         state.setLevel4(0);
