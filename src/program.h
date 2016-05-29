28191fc6d08e905ebe91b8e1c9655036cc48251c
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-05-29 11:09:17 +0100

    use a new sub-interpreter for every reload, so that all imported modules are refreshed

--- src/program.h
+++ src/program.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <python3.4m/Python.h>
+#include <cassert>
 #include <cstdio>
 
 #include "config.h"
@@ -9,22 +10,56 @@
 
 class Interpreter {
 
-public:
+    char const * const _programPath;
 
-    Interpreter(Config const& config) {
-        Py_InitializeEx(0);
-        debug("Loading: %s", config._programPath);
-        auto const f = fopen(config._programPath, "r");
+    PyThreadState *_main, *_current;
+
+    void newInterpreter() {
+        debug("Creating new sub-interpreter.");
+        _current = Py_NewInterpreter();
+        assert(_current);
+        assert(PyThreadState_Get() == _current);
+    }
+
+    void endInterpreter() {
+        debug("Ending sub-interpreter.");
+        Py_EndInterpreter(_current);
+    }
+
+    void load() {
+        newInterpreter();
+        debug("Loading: %s", _programPath);
+        auto const f = fopen(_programPath, "r");
         if (f) {
-            PyRun_SimpleFile(f, config._programPath);
+            PyRun_SimpleFile(f, _programPath);
             fclose(f);
         }
         else {
-            debug("Failed to open: %s", config._programPath);
+            debug("Failed to open: %s", _programPath);
         }
     }
 
+public:
+
+    Interpreter(Config const& config)
+            : _programPath(config._programPath) {
+        debug("Initing Python.");
+        Py_InitializeEx(0);
+        PyEval_InitThreads();
+        _main = PyThreadState_Get();
+        assert(_main);
+        load();
+    }
+
+    void reload() {
+        endInterpreter();
+        load();
+    }
+
     ~Interpreter() {
+        endInterpreter();
+        PyThreadState_Swap(_main); // Otherwise Py_Finalize crashes.
+        debug("Closing Python.");
         Py_Finalize();
     }
 
