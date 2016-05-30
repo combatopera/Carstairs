56797bdb2fdfbf7ea5ea3beacdd5a4cc529bd9d2
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-05-30 16:32:21 +0100

    too spammy for debug

--- src/util/py.cpp
+++ src/util/py.cpp
@@ -2,9 +2,11 @@
 
 #include <python3.4m/Python.h>
 
+#include "util.h"
+
 static Log const LOG(__FILE__);
 
 void PyRef::xdecref() const {
-    debug("XDECREF: %p", _ptr);
+    trace("XDECREF: %p", _ptr);
     Py_XDECREF(_ptr);
 }
