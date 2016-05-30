47fe0d0cdeb3d4f650bfb351e525cbbd88a975ed
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-05-30 12:22:04 +0100

    auto

--- src/program.cpp
+++ src/program.cpp
@@ -8,12 +8,12 @@ void Program::fire(int noteFrame, int offFrameOrNeg, State& state) const {
     }
     if (_module) {
         if (offFrameOrNeg < 0) {
-            PyRef on = _module.getAttr("on");
+            auto const on = _module.getAttr("on");
             PyRef args = Py_BuildValue("(i)", noteFrame);
             Py_XDECREF(PyEval_CallObject(on, args));
         }
         else {
-            PyRef off = _module.getAttr("off");
+            auto const off = _module.getAttr("off");
             PyRef args = Py_BuildValue("ii", noteFrame, offFrameOrNeg);
             Py_XDECREF(PyEval_CallObject(off, args));
         }
