75ad2b621284dd683cdb3336098195d7bbf0722e
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-05-31 11:21:56 +0100

    use anon ns instead of static

--- src/dssi/plugin.cpp
+++ src/dssi/plugin.cpp
@@ -8,48 +8,54 @@
 #include "../util/util.h"
 #include "port.h"
 
-static Log const LOG(__FILE__);
+namespace {
 
-static Config const CONFIG; // Must be in same file as PortInfo for static init order.
+Log const LOG(__FILE__);
 
-static PortInfoEnum const PortInfo {CONFIG}; // Must be in same file as descriptor for static init order.
+Config const CONFIG; // Must be in same file as PortInfo for static init order.
 
-static LADSPA_Handle instantiate(const LADSPA_Descriptor *Descriptor, DSSI::cursor SampleRate) {
-    debug("LADSPA: instantiate");
+PortInfoEnum const PortInfo {CONFIG}; // Must be in same file as descriptor for static init order.
+
+Descriptors const descriptors;
+
+LADSPA_Handle instantiate(const LADSPA_Descriptor *Descriptor, DSSI::cursor SampleRate) {
+    debug("LADSPA: instantiate(%lu)", SampleRate);
     return new dizzYM(CONFIG, PortInfo, int(SampleRate));
 }
 
-static void activate(LADSPA_Handle Instance) {
+void activate(LADSPA_Handle Instance) {
     debug("LADSPA: activate");
     static_cast<dizzYM *>(Instance)->start();
 }
 
-static void deactivate(LADSPA_Handle Instance) {
+void deactivate(LADSPA_Handle Instance) {
     debug("LADSPA: deactivate");
 }
 
-static void connect_port(LADSPA_Handle Instance, DSSI::cursor Port, LADSPA_Data *DataLocation) {
-    debug("LADSPA: connect_port");
+void connect_port(LADSPA_Handle Instance, DSSI::cursor Port, LADSPA_Data *DataLocation) {
+    debug("LADSPA: connect_port(%lu)", Port);
     static_cast<dizzYM *>(Instance)->setPortValPtr((int) Port, DataLocation);
 }
 
-static int get_midi_controller_for_port(LADSPA_Handle, DSSI::cursor Port) {
-    debug("DSSI: get_midi_controller_for_port");
+int get_midi_controller_for_port(LADSPA_Handle, DSSI::cursor Port) {
+    debug("DSSI: get_midi_controller_for_port(%lu)", Port);
     return PortInfo._values.at(sizex(Port))->_controllers;
 }
 
-static void run(LADSPA_Handle Instance, DSSI::cursor SampleCount) {
+void run(LADSPA_Handle Instance, DSSI::cursor SampleCount) {
     static_cast<dizzYM *>(Instance)->runSynth(SampleCount, 0, 0);
 }
 
-static void run_synth(LADSPA_Handle Instance, DSSI::cursor SampleCount, snd_seq_event_t *Events, DSSI::cursor EventCount) {
+void run_synth(LADSPA_Handle Instance, DSSI::cursor SampleCount, snd_seq_event_t *Events, DSSI::cursor EventCount) {
     static_cast<dizzYM *>(Instance)->runSynth(SampleCount, Events, EventCount);
 }
 
-static void cleanup(LADSPA_Handle Instance) {
+void cleanup(LADSPA_Handle Instance) {
     debug("LADSPA: cleanup");
     delete static_cast<dizzYM *>(Instance);
     debug("Cleaned up.");
+}
+
 }
 
 Descriptors::Descriptors() {
@@ -107,8 +113,6 @@ Descriptors::~Descriptors() {
     delete[] _PortNames;
     delete[] _PortRangeHints;
 }
-
-static Descriptors const descriptors;
 
 extern "C" {
 
