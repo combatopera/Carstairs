0bde0dfd1fa216ce38ba7dced4981f2ec83aa686
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-06-01 11:12:20 +0100

    fix

--- src/dssi/plugin.cpp
+++ src/dssi/plugin.cpp
@@ -18,26 +18,26 @@ PortInfoEnum const PortInfo {CONFIG}; // Must be in same file as descriptor for 
 Descriptors const descriptors {CONFIG};
 
 LADSPA_Handle instantiate(const LADSPA_Descriptor *Descriptor, DSSI::cursor SampleRate) {
-    debug("LADSPA: instantiate(%lu)", SampleRate);
+    CARSTAIRS_DEBUG("LADSPA: instantiate(%lu)", SampleRate);
     return new Carstairs(CONFIG, PortInfo, int(SampleRate));
 }
 
 void activate(LADSPA_Handle Instance) {
-    debug("LADSPA: activate");
+    CARSTAIRS_DEBUG("LADSPA: activate");
     static_cast<Carstairs *>(Instance)->start();
 }
 
 void deactivate(LADSPA_Handle Instance) {
-    debug("LADSPA: deactivate");
+    CARSTAIRS_DEBUG("LADSPA: deactivate");
 }
 
 void connect_port(LADSPA_Handle Instance, DSSI::cursor Port, LADSPA_Data *DataLocation) {
-    debug("LADSPA: connect_port(%lu)", Port);
+    CARSTAIRS_DEBUG("LADSPA: connect_port(%lu)", Port);
     static_cast<Carstairs *>(Instance)->setPortValPtr((int) Port, DataLocation);
 }
 
 int get_midi_controller_for_port(LADSPA_Handle, DSSI::cursor Port) {
-    debug("DSSI: get_midi_controller_for_port(%lu)", Port);
+    CARSTAIRS_DEBUG("DSSI: get_midi_controller_for_port(%lu)", Port);
     return PortInfo._values.at(sizex(Port))->_controllers;
 }
 
@@ -50,9 +50,9 @@ void run_synth(LADSPA_Handle Instance, DSSI::cursor SampleCount, snd_seq_event_t
 }
 
 void cleanup(LADSPA_Handle Instance) {
-    debug("LADSPA: cleanup");
+    CARSTAIRS_DEBUG("LADSPA: cleanup");
     delete static_cast<Carstairs *>(Instance);
-    debug("Cleaned up.");
+    CARSTAIRS_DEBUG("Cleaned up.");
 }
 
 }
@@ -99,7 +99,7 @@ Descriptors::Descriptors(Config const& config) {
         0,// run_multiple_synths()
         0,// run_multiple_synths_adding()
     };
-    debug("Constructed the Descriptors.");
+    CARSTAIRS_DEBUG("Constructed the Descriptors.");
 }
 
 DSSI_Descriptor const *Descriptors::dssiDescriptor() const {
@@ -107,7 +107,7 @@ DSSI_Descriptor const *Descriptors::dssiDescriptor() const {
 }
 
 Descriptors::~Descriptors() {
-    debug("Destroying the Descriptors.");
+    CARSTAIRS_DEBUG("Destroying the Descriptors.");
     delete[] _PortDescriptors;
     delete[] _PortNames;
     delete[] _PortRangeHints;
