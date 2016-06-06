647b2facfb66348a5b5da85efffef8b3b7db65f1
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-06-06 08:21:08 +0100

    test

--- test/node/noise.cxx
+++ test/node/noise.cxx
@@ -17,6 +17,7 @@ BOOST_AUTO_TEST_CASE(LFSR) {
     for (auto n = expected.size(), i = n - n; i < n; ++i) {
         expected[i] = 1 - expected[i]; // According to qnoispec, raw LFSR 1 maps to amp 0, so we flip our LFSR.
     }
+    BOOST_REQUIRE_EQUAL((1 << 17) - 1, noiseShape().limit());
     for (sizex kMax = noiseShape().limit() - sizex(expected.size()), k = kMax - kMax; k < kMax; ++k) {
         bool match = true;
         for (auto n = expected.size(), i = n - n; i < n; ++i) {
@@ -30,6 +31,32 @@ BOOST_AUTO_TEST_CASE(LFSR) {
         }
         else {
             BOOST_CHECK_NE(10221, k);
+        }
+    }
+}
+
+struct F {
+
+    Config _config;
+
+    F() {
+        _config._atomSize = 8;
+    }
+
+};
+
+BOOST_FIXTURE_TEST_CASE(works, F) {
+    State state(_config);
+    state._NP = 3;
+    Noise o(_config, state);
+    o.start();
+    auto n = 100, x = 0;
+    Buffer<int> expected("expected", 48);
+    for (auto _ = 0; _ < 2; ++_) {
+        auto v = o.render(o.cursor() + 48 * n);
+        for (auto i = 0; i < n; ++i) {
+            expected.fill(noiseShape().at(x++));
+            BOOST_CHECK_EQUAL_COLLECTIONS(expected.begin(), expected.end(), v.begin(i * 48), v.begin((i + 1) * 48));
         }
     }
 }
