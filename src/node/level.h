fe1760cc35de7c9260092d2d05e80ef9f4551ab0
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-06-09 19:30:44 +0100

    implement variable level

--- src/node/level.h
+++ src/node/level.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <cmath>
 
 #include "../config.h"
@@ -11,30 +12,47 @@
 
 class Level: public Node<float> {
 
-    float const _maxAmp;
+    std::array<float, 32> _level5ToAmp;
+
+    bool const& _levelMode;
 
     Mixer& _mixer;
 
     Env& _env;
-
-    bool const& _levelMode;
 
     void startImpl() {
         // Nothing to be done.
     }
 
     void renderImpl() {
-        auto const amp = powf(2, (float(_state.level5()) - 31) / 4) * _maxAmp;
-        auto const binary = _mixer.render(cursor() + _buf.limit());
-        for (auto i = _buf.limit() - 1; SIZEX_NEG != i; --i) {
-            _buf.put(i, float(binary.at(i) * 2 - 1) * amp);
+        auto const n = _buf.limit();
+        auto const newCursor = cursor() + n;
+        auto const binary = _mixer.render(newCursor);
+        auto binPtr = binary.begin();
+        auto outPtr = const_cast<float *>(_buf.begin());
+        if (_levelMode) {
+            auto const env = _env.render(newCursor);
+            auto level5Ptr = env.begin();
+            for (auto i = n - 1; SIZEX_NEG != i; --i) {
+                *outPtr++ = float(*binPtr++ * 2 - 1) * _level5ToAmp[*level5Ptr++];
+            }
+        }
+        else {
+            auto const amp = _level5ToAmp[_state.level5()];
+            for (auto i = n - 1; SIZEX_NEG != i; --i) {
+                *outPtr++ = float(*binPtr++ * 2 - 1) * amp;
+            }
         }
     }
 
 public:
 
-    Level(Config const& config, State& state, Mixer& mixer, Env& env)
-            : Node("Level", state), _maxAmp(config._maxAmp), _mixer(mixer), _env(env), _levelMode(state.levelMode()) {
+    Level(Config const& config, State const& state, Mixer& mixer, Env& env)
+            : Node("Level", state), _levelMode(state.levelMode()), _mixer(mixer), _env(env) {
+        auto const maxLevel5 = sizex(_level5ToAmp.size() - 1);
+        for (auto level5 = maxLevel5; SIZEX_NEG != level5; --level5) {
+            _level5ToAmp[level5] = powf(2, float(maxLevel5 - level5) / -4) * config._maxAmp;
+        }
     }
 
 };
