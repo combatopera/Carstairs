b6636354140ea01b0eee1864ca86d30353ab3bf0
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-06-09 08:30:11 +0100

    inline

--- src/node/level.h
+++ src/node/level.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <cmath>
+
 #include "../config.h"
 #include "../node.h"
 #include "../state.h"
+#include "../util/buf.h"
 #include "mixer.h"
 
 class Level: public Node<float> {
@@ -12,9 +15,16 @@ class Level: public Node<float> {
     Mixer& _mixer;
 
     void startImpl() {
+        // Nothing to be done.
     }
 
-    void renderImpl();
+    void renderImpl() {
+        auto const amp = powf(2, (float(_state.level5()) - 31) / 4) * _maxAmp;
+        auto const binary = _mixer.render(cursor() + _buf.limit());
+        for (auto i = _buf.limit() - 1; SIZEX_NEG != i; --i) {
+            _buf.put(i, float(binary.at(i) * 2 - 1) * amp);
+        }
+    }
 
 public:
 
