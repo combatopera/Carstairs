ebe915c7e86f01e84a289e130a7c5c0bb6c85573
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-06-10 09:17:03 +0100

    the 4 useful env shapes

--- src/node/env.cpp
+++ src/node/env.cpp
@@ -9,13 +9,54 @@ class Env1000: public Buffer<int> {
 public:
 
     Env1000()
-            : Buffer("Env1000", 32) {
+            : Buffer("ENV_1000", 32) {
         for (auto i = 0; i < 32; ++i) {
             put(i, 31 - i);
         }
     }
 
 } ENV_1000;
+
+class Env1010: public Buffer<int> {
+
+public:
+
+    Env1010()
+            : Buffer("ENV_1010", 64) {
+        for (auto i = 0; i < 32; ++i) {
+            put(i, 31 - i);
+            put(32 + i, i);
+        }
+    }
+
+} ENV_1010;
+
+class Env1100: public Buffer<int> {
+
+public:
+
+    Env1100()
+            : Buffer("ENV_1100", 32) {
+        for (auto i = 0; i < 32; ++i) {
+            put(i, i);
+        }
+    }
+
+} ENV_1100;
+
+class Env1110: public Buffer<int> {
+
+public:
+
+    Env1110()
+            : Buffer("ENV_1110", 64) {
+        for (auto i = 0; i < 32; ++i) {
+            put(i, i);
+            put(32 + i, 31 - i);
+        }
+    }
+
+} ENV_1110;
 
 }
 
