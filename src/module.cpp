83293f427cd8e1335f159e8b4c37bcf4e524877b
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-06-12 11:54:16 +0100

    define the envelope shapes

--- src/module.cpp
+++ src/module.cpp
@@ -37,6 +37,25 @@ char const * const CODE =
         n = len(self.v)
         return self.v[i] if i < n else self.v[n - 1]
 
+class EnvShape:
+
+    def __init__(self, att_alt_hold):
+        alt_hold = att_alt_hold & 0b11
+        if 0 == alt_hold: # Saw.
+            self.wavelength = 32
+        elif 2 == alt_hold: # Triangle.
+            self.wavelength = 64
+
+desc_saw = EnvShape(0)
+desc_hold = EnvShape(1)
+desc_tri = EnvShape(2)
+desc_flip = EnvShape(3)
+asc_saw = EnvShape(4)
+asc_hold = EnvShape(5)
+asc_tri = EnvShape(6)
+asc_flip = EnvShape(7)
+del EnvShape
+
 class Chip:
 
     clock = %2%
@@ -61,7 +80,7 @@ del Note
 
 class Program:
 
-    rate = 50
+    rate = 50 # Default if the program doesn't set one.
 
 program = Program()
 del Program
