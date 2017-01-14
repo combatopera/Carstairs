6e5433b7b53410a8a96da1ace9e53be930409505
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-01-14 12:03:27 +0000

    what's it doing

--- drone.io.sh
+++ drone.io.sh
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Carstairs.  If not, see <http://www.gnu.org/licenses/>.
 
-set -e
+set -ex
 
 git clone https://github.com/combatopera/pyven.git ../pyven
 
