c7ee192695d8b84b1c13e24dc8b7436feec17d09
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-01-31 15:54:18 +0000

    require sudo, maybe gcc is ok

--- drone.io.sh
+++ drone.io.sh
@@ -21,7 +21,7 @@ set -ex
 
 ../pyven/drone.io.py
 
-echo 2 | sudo update-alternatives --config gcc
+#echo 2 | sudo update-alternatives --config gcc
 
 sudo apt-get install dssi-dev libboost1.48-dev libboost-filesystem1.48.0 libboost-test1.48.0 python3-dev libfftw3-dev cppcheck zip
 
