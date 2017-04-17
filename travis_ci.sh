afbd22dd3d1d333d5b31f22eaaa99f4fdd49eb05
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-04-17 03:10:11 +0100

    build expects python 3.4

--- travis_ci.sh
+++ travis_ci.sh
@@ -23,6 +23,6 @@ sudo apt-get install python-pyparsing
 
 ../pyven/travis_ci.py
 
-sudo apt-get install dssi-dev libboost1.55-dev libboost-filesystem1.55.0 libboost-test1.55.0 libfftw3-dev cppcheck scons
+sudo apt-get install dssi-dev libboost1.55-dev libboost-filesystem1.55.0 libboost-test1.55.0 libfftw3-dev cppcheck scons python3.4
 
 scons
