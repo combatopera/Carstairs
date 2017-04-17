5be6722f42b45cb3d7dd0126fbcdd6f0326f34a6
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-04-17 03:14:41 +0100

    hmmm

--- travis_ci.sh
+++ travis_ci.sh
@@ -23,6 +23,8 @@ sudo apt-get install python-pyparsing
 
 ../pyven/travis_ci.py
 
-sudo apt-get install dssi-dev libboost1.55-dev libboost-filesystem1.55.0 libboost-test1.55.0 libfftw3-dev cppcheck scons python3.4
+sudo apt-get install dssi-dev libboost1.55-dev libboost-filesystem1.55.0 libboost-test1.55.0 libfftw3-dev cppcheck scons
+
+ls -l /usr/bin/python*config
 
 scons
